e3b340d34a687903f24cc755dd60e879c802a796
Satyaki Suman <satyaki.suman@gmail.com>
2020-07-24 13:09:47 +0530

    pretty

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
-  
-const express = require('express')
-const app = express()
+require("dotenv").config();
 
-// require('dotenv').config()
+const express = require("express");
+const app = express();
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true , useUnifiedTopology: true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
